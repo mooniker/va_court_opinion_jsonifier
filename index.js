d00eb15ccbfb4105045dc50ef916e5cb45edfab7
James Moon <mooniker@gmail.com>
2015-12-22 16:29:50 -0500

    Add route to activate the scrape

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ var app = express(); // initialize our web application's server
 app.listen( 4000, function() {
   console.log('Hit up port 4000 to start the action.');
 } );
+
+app.get( '/scrape', function( req, res ) {
+
+  // target for our scraping, opinions from Virginia's highest state court
+  var url = 'http://www.courts.state.va.us/scndex.htm';
+
+  console.log('Scraping target:', url);
+
+  // here we want to request the webpage at the given url
+
+  // and then we want to craft JSON from the info we extract
+
+} );
