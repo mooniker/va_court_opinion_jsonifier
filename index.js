3e3f1d96562062f724c40343260752b5af1fb35f
James Moon <mooniker@gmail.com>
2015-12-22 17:55:36 -0500

    Add request to get webpage's HTML

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ app.get( '/scrape', function( req, res ) {
 
   // here we want to request the webpage at the given url
 
-  // and then we want to craft JSON from the info we extract
+  request( url, function( error, response, html ) {
+
+    if ( !error && response.statusCode == 200 ) { // status code 200 means OK
+
+      // and then we want to craft JSON from the info we extract
+
+      res.send(html); // or, instead, just show us the HTML we got for now
+
+    } else if (error) { // if we fail to get a response from the Virginia server
+      console.error(error);
+    } else { // if the status code of the response is anything other than OK
+      console.log(response);
+    }
+
+  } );
 
 } );
