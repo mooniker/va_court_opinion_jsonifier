53b761b8803cb1a55c4109a90825426861a3f9e2
James Moon <mooniker@gmail.com>
2015-12-22 22:14:27 -0500

    Add string manipulations to pull out case name, docket, sumamry, date, and hrefs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var request = require('request'); // HTTP client - to get input
 var express = require('express'); // HTTP server - middle of the action
 var cheerio = require('cheerio'); // power up jQuery on the server
+var path    = require('path');    // lets us craft absolute URLs when needed
 
 var app = express(); // initialize our web application's server
 
@@ -21,7 +22,7 @@ app.get( '/scrape', function( req, res ) {
 
     if ( !error && response.statusCode == 200 ) { // status code 200 means OK
 
-      // initialize Cheerio with requested HTML
+      // Initialize Cheerio with requested HTML
       var $ = cheerio.load(html); // allows DOM transversal jQuery-style on $
 
       var $opinions = $('p').slice(2); // get all p elements, discard first two
@@ -30,13 +31,42 @@ app.get( '/scrape', function( req, res ) {
       var jsons = []; // initialize array for collecting all our JSONs
 
       for ( var p = 0; p < $opinions.length; p += 1 ) {
+
+        // Grab all text inside p element
         var pContent = $opinions.eq(p).text();
 
-        // crafting JSON from the info extracted from each element in $opinions
-        // would happen here
+        // Grab text inside b element (bold tags)
+        var caseName = $opinions.eq(p).find('b').text();
+
+        // Grab the first word in the text (and assume it's a docket)
+        var docketNumber = pContent.trim().split(' ')[0];
+
+        // Split text on line break and assume the second part is summary
+        var summary = pContent.split('\n')[1].trim();
+
+        // Split text on line break and assume first part is meta info
+        var metaInfo = pContent.split('\n')[0].trim();
+        // Grab the last word in meta info and assume it's the date
+        var date = metaInfo.split(' ')[metaInfo.split(' ').length - 1];
+
+        // Find all the a elements that have an href attribute
+        var $aWithHrefs = $opinions.eq(p).find('a[href]');
+        var hrefs = []; // compile the href json in this
+        // iterate through all the a[href] elements and extract the link info
+        for ( var a = 0; a < $aWithHrefs.length; a += 1 ) {
+          hrefs.push({
+            name: $aWithHrefs.eq(a).text(),
+            href: path.join( path.dirname(url), $aWithHrefs.eq(a).attr('href') )
+          });
+        }
 
         jsons.push({
-          content: pContent // let's just push the contents of each p into JSON
+          case_name: caseName,
+          docket_number: docketNumber,
+          date: date,
+          summary: summary,
+          hrefs: hrefs,
+          // content: pContent // let's just push the contents of each p into JSON
         });
       }
 
